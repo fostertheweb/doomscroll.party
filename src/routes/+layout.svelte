3cfc1de7e018117a3cb531d4926c1b833d9e6025
Jonathan Foster <fostertheweb@gmail.com>
2023-11-11 22:41:36 -0500

    tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<script async src="https://www.tiktok.com/embed.js"></script>
+</svelte:head>
+
 <slot />
---
+++ src/routes/party/[id]/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let data;
+
+	let video: HTMLVideoElement;
+
+	function play() {
+		console.log('loadeddata');
+	}
+
+	function end() {
+		console.log('ended');
+	}
+</script>
+
+<div class="flex h-screen w-screen items-center justify-center bg-slate-800">
+	<video
+		class="max-h-screen rounded-lg shadow-lg"
+		autoplay
+		bind:this={video}
+		on:loadeddata={play}
+		on:ended={end}
+		src={data.video}
+	>
+		<track kind="captions" />
+	</video>
+</div>
+
+<style>
+	video {
+		max-height: 80vh;
+	}
+</style>
